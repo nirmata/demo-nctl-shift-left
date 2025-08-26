571aad8c1f468881a813203cb8df92e4561f3c25
Mayur Kotamraju <mayur@baffle.io>
2025-08-26 10:58:13 -0500

    Flipping flag for demo

--- config-files/terraform/eks/eks.tf
+++ config-files/terraform/eks/eks.tf
@@ -7,7 +7,7 @@ module "eks" {
   cluster_version = var.cluster_version
 
   cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access  = false
 
   cluster_encryption_config = {
     resources = ["secrets"]

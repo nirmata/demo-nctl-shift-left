818a1f5ef20b4f87283b4b56f346ec401453fb2d
Jigar Joshi <jigar.joshi@nirmata.com>
2025-12-15 17:01:39 +0530

    pushing value to fail tf scan

--- config-files/terraform/eks/eks.tf
+++ config-files/terraform/eks/eks.tf
@@ -6,7 +6,7 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  cluster_endpoint_private_access = false
+  cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
 
   cluster_encryption_config = {
